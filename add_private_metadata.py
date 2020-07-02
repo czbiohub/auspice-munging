32df143d8209a3fc15b22a6c9dea6c4c5a9866ad
Joshua Batson <joshua.batson@czbiohub.org>
2020-07-01 19:13:56 -0700

    Split out munging into adding metadata and trimming tree

---
+++ add_private_metadata.py
@@ -0,0 +1,112 @@
+#!/usr/bin/env python3
+
+import argparse
+import pandas as pd
+import os
+from tree import *
+import copy
+
+def read_table(filename):
+    if filename.endswith('csv'):
+        return pd.read_csv(filename, dtype=str)
+    if filename.endswith('tsv'):
+        return pd.read_csv(filename, sep = '\t', dtype=str)
+    if filename.endswith('xls') or filename.endswith('xlsx'):
+        return pd.read_excel(filename, dtype=str)
+    else:
+        raise NotImplementedError
+
+def update_json_meta(js, tree, df, county):
+    for key in list(df.columns):
+        if key == 'age':
+            continue
+        order_type = 'categorical'
+        # if key in ['rooms', 'num_in_hh']:
+        #     order_type = 'continuous'
+        coloring = {'key': key, 'title': key.capitalize(), 'type': order_type}
+        js['meta']['colorings'].insert(0, coloring)
+        js['meta']['filters'].insert(0, key)
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--json',
+                        help='auspice json')
+    parser.add_argument('--submitted-sequences',
+                        help='CSV with GISAID ID, CZB_ID')
+    parser.add_argument('--dph-ids',
+                        help='CSV with DPH ID, CZB_ID')
+    parser.add_argument('--metadata',
+                        help='External metadata')
+    parser.add_argument('--county',
+                        help='Restrict metadata additions to county. If no county '
+                             'is specified, add metadata to all entries.', default=None)
+    parser.add_argument('--output',
+                        help='Output file. If none, the format will be'
+                             ' COUNTY_SCALE.json',
+                        default=None)
+    parser.add_argument('--node-name',
+                        help='Metadata field to rename nodes by.',
+                        default=None)
+
+    args = parser.parse_args()
+
+    with open(args.json, 'r') as fp:
+        js = json.load(fp)
+    tree = Tree(js['tree'])
+
+    gisaid_to_czb = read_table(args.submitted_sequences)
+    gisaid_to_czb['strain'] = gisaid_to_czb['gisaid_name'].apply(lambda x: x[8:] if isinstance(x, str) else x)
+
+    czb_to_dph = read_table(args.dph_ids)
+
+    df = gisaid_to_czb[['strain', 'CZB_ID']].merge(
+            czb_to_dph[['CZB_ID', 'Partner_ID']],
+            on='CZB_ID',
+            how='left')
+
+    if args.metadata:
+        metadata = read_table(args.metadata)
+        metadata.rename({col: '_'.join(col.split()) for col in metadata.columns}, axis=1,
+                  inplace=True)
+        if 'Partner_ID' in metadata.columns:
+            key = 'Partner_ID'
+        elif 'strain' in metadata.columns:
+            key = 'strain'
+        else:
+            raise ValueError
+        df = df.merge(metadata, on=key, how='left')
+
+        for field in metadata.columns:
+            if field not in ['strain', 'Partner_ID', 'CZB_ID']:
+                coloring = {'key': field, 'title': field.capitalize(),
+                            'type': 'categorical'}
+                js['meta']['colorings'].insert(0, coloring)
+                js['meta']['filters'].insert(0, field)
+
+    if args.county:
+        samples_to_add_metadata_to = [n.name for n in tree.filter_nodes('county', args.county)]
+        df = df[df['strain'].isin(samples_to_add_metadata_to)]
+    tree.add_metadata(df)
+
+    if args.node_name:
+        tree.rename_nodes(args.node_name, save_attr='GISAID_ID')
+
+    if args.output is None:
+        outfile = (os.path.dirname(args.json) + '/'
+                   + 'with_metadata_for_'
+                   + ("_".join(args.county.split()) if args.county else "all")
+                   + '.json')
+        print(outfile)
+    else:
+        outfile = args.output
+    with open(outfile, 'w') as fp:
+        json.dump(
+            {"meta": js['meta'],
+             "version": js['version'],
+             "tree": tree.to_dict()},
+            fp,
+            indent=2)
+
+if __name__ == '__main__':
+    main()
---
+++ trim.py
@@ -0,0 +1,80 @@
+#!/usr/bin/env python3
+
+import argparse
+import pandas as pd
+import os
+from tree import *
+import copy
+
+def trim_tree(tree, county, scale):
+    if scale is None:
+        return tree
+
+    tree = copy.deepcopy(tree)
+
+    if scale == 'local':
+        leaves = tree.filter_nodes('county', county)
+        nodes_to_keep = walk_to_root(leaves)
+    elif scale == 'ancestors':
+        leaves = tree.filter_nodes('county', county)
+        nodes_to_keep = walk_to_root(leaves)
+        nodes_to_keep = walk_down(nodes_to_keep, mode='mutations', depth=0)
+        nodes_to_keep = walk_to_root(
+            [n for n in nodes_to_keep if n.type == NodeType.LEAF])
+    if tree.root not in nodes_to_keep:
+        nodes_to_keep.append(tree.root)
+
+    n_genomes = len([n for n in nodes_to_keep if n.type == NodeType.LEAF])
+    print("Subsetting tree to ", n_genomes, " genomes.")
+    tree.subset_tree(nodes_to_keep)
+    return tree
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--json',
+                        help='auspice json')
+    parser.add_argument('--county',
+                        help='County to filter on. If no county is specified,'
+                             'no filtering will be done.', default=None)
+    parser.add_argument('--scale',
+                        help='Sets scale of analysis. `local` will show only '
+                             'sequences from the county. `ancestors` will show '
+                             'sequences from county and plausible '
+                             'ancestors. ',
+                        default=None)
+    parser.add_argument('--output',
+                        help='Output file. If none, the format will be'
+                             ' COUNTY_SCALE.json',
+                        default=None)
+
+    args = parser.parse_args()
+
+    with open(args.json, 'r') as fp:
+        js = json.load(fp)
+    tree = Tree(js['tree'])
+
+    tree = trim_tree(tree, args.county, args.scale)
+
+    if args.county:
+        js['meta']['title'] = 'COVID Tracker CA: ' + args.county
+
+    if args.output is None:
+        outfile = (os.path.dirname(args.json) + '/'
+                   + ("_".join(args.county.split()) if args.county else "all")
+                   + '_'
+                   + str(args.scale)
+                   + '.json')
+        print(outfile)
+    else:
+        outfile = args.output
+    with open(outfile, 'w') as fp:
+        json.dump(
+            {"meta": js['meta'],
+             "version": js['version'],
+             "tree": tree.to_dict()},
+            fp,
+            indent=2)
+
+
+if __name__ == '__main__':
+    main()
